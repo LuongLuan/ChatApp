4bbbb65bfb810f5e9e61a6fd2c64faac23e9bc8a
Luan Luong Van (SV) <luan.luong.van@gosell.vn>
2025-05-27 17:53:39 +0700

    add docker dcproj

--- Post.Api/Program.cs
+++ Post.Api/Program.cs
@@ -1,36 +1,122 @@
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Post.Api.Errors;
+using Post.Api.GrpcService;
+using Post.Api.Mapper;
+using Post.Api.Services;
+using Post.Core.Interfaces;
+using Post.Infrastructure;
+using Post.Infrastructure.Data;
+using User.Grpc.Protos;
 
-namespace Post.Api
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddTransient<IIdentityService, IdentityService>();
+
+builder.Services.AddAuthentication("Bearer")
+	.AddJwtBearer("Bearer", options =>
+	{
+		options.RequireHttpsMetadata = false;
+		options.Authority = builder.Configuration["IdentityServer:BaseUrl"]; //url Identity Server
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateAudience = false,
+			ValidateIssuer = false// fix 401 response in docker
+		};
+	});
+
+// [Authorize("ClientIdPolicy")]
+/*builder.Services.AddAuthorization(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    options.AddPolicy("ClientIdPolicy", policy => policy.RequireAuthenticatedUser());
+});*/
 
-            // Add services to the container.
+builder.Services.AddScoped<UserGrpcService>();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+builder.Services.AddGrpcClient<UserProtoService.UserProtoServiceClient>
+	(o => o.Address = new Uri(builder.Configuration["GrpcSettings:UserUrl"]));
 
-            var app = builder.Build();
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config => {
+	config.UsingRabbitMq((ctx, cfg) => {
+		cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+		//cfg.UseHealthCheck(ctx);
+	});
+});
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  builder =>
+					  {
+						  builder.WithOrigins("http://localhost:4200")
+						  .AllowAnyHeader()
+						  .AllowAnyMethod()
+						  .AllowCredentials();
+					  });
+});
 
-            app.UseHttpsRedirection();
+// validation error reponse
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+	options.InvalidModelStateResponseFactory = actionContext =>
+	{
+		var errors = actionContext.ModelState
+			.Where(e => e.Value.Errors.Count > 0)
+			.SelectMany(x => x.Value.Errors)
+			.Select(x => x.ErrorMessage).ToArray();
 
-            app.UseAuthorization();
+		var errorResponse = new ApiValidationErrorResponse
+		{
+			Errors = errors
+		};
 
+		return new BadRequestObjectResult(errorResponse);
+	};
+});
 
-            app.MapControllers();
+var app = builder.Build();
 
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+	try
+	{
+		var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+		await context.Database.MigrateAsync();
+		await Seed.SeedPostAsync(context);
+	}
+	catch (Exception ex)
+	{
+		app.Logger.LogError(ex, "An error occurred during migration");
+	}
+}
+
+app.Run();
